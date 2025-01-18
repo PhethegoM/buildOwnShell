2afef9952d844d296bdeb89dde0afe45068737e1
Phethego Motidi <phethegomotidi@gmail.com>
2025-01-18 17:51:53 +0200

    The exit builtin

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 import sys
 
-
 def main():
-
-    
 
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
 
-        command = input()
+        command = input().rstrip()
 
-        print(f'{command}: command not found')
+        # try:
+        #     command, int = command.split()
+        # except:
+        #     command = command
+
+        if command == 'exit 0':
+            sys.exit()
+        else:
+            print(f'{command}: command not found')
 
 
 if __name__ == "__main__":
