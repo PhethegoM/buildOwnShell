fc08437f7925dd57aecc7b5a85376fb9f4700751
Phethego Motidi <phethegomotidi@gmail.com>
2025-01-28 10:46:13 +0200

    Type builtin

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 
 def main():
+    commands = ['exit', 'echo', 'type']
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
@@ -9,11 +10,23 @@ def main():
 
         if command.split()[0] == 'exit' and command.split()[1] == '0':
                 sys.exit()
+
         if command.split()[0] == 'echo':
              command = ' '.join(command.split()[1:])
              sys.stdout.write(f'{command}\n')
-        else:
+             continue
+        
+        if command.split()[0] == 'type':
+             if command.split()[1] in commands:
+                  sys.stdout.write(f'{command.split()[1]} is a shell builtin\n')
+                  continue
+             else:
+                sys.stdout.write(f'{command.split()[1]}: not found\n')
+                continue
+                  
+        if command.split()[0] not in commands:
             sys.stdout.write(f'{command}: command not found\n')
+            continue
 
 
 
