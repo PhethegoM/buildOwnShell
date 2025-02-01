08c27f7b107695cf599005862144f4c9f595970f
Phethego Motidi <phethegomotidi@gmail.com>
2025-02-01 12:35:12 +0200

    Run external program

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ def main():
                 output, error = process.communicate()
 
                 if output:
-                    print(output.decode('utf-8').split('\n')[0])
+                    print(output.decode('utf-8'))
 
                 if error:
                     print(error)
